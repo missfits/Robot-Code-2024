26265e3d51bb57e5ce8fbb03674150a398d9fb40
kaileyhh <kaileyhua@gmail.com>
2023-07-01 14:22:24 -0700

    fix subsystem template names

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -7,8 +7,8 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Shooter extends SubsystemBase {
-  public Shooter() {}
+public class Drivetrain extends SubsystemBase {
+  public Drivetrain() {}
 
   @Override
   public void periodic() {}
