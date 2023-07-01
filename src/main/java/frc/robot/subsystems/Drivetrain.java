513d87d9450fc92b77faea068d0b699c15989b23
kaileyhh <kaileyhua@gmail.com>
2023-07-01 03:29:26 -0700

    organize constants

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -6,9 +6,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.DrivetrainConstants;
 
-public class Shooter extends SubsystemBase {
-  public Shooter() {}
+
+public class Drivetrain extends SubsystemBase {
+  public Drivetrain() {}
 
   @Override
   public void periodic() {}
