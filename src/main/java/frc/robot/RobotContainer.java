e9591b8b428f4c22d76ff5373bd5e0fbee58e872
amzoeee <zoe.z.zhao@gmail.com>
2024-01-22 18:39:58 -0800

    add default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,6 +30,7 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
+    m_exampleSubsystem.setDefaultCommand(new ExampleCommand(m_exampleSubsystem));
     configureBindings();
   }
 
