9b8feccc7afe338fa6a2e817bf5f1230fc78145c
amzoeee <zoe.z.zhao@gmail.com>
2024-01-26 08:58:57 -0800

    add code to restore spark max to factory default
    suggested by CD thread: https://www.chiefdelphi.com/t/certain-spark-maxes-dont-work-through-can-bus/452062/9

--- src/main/java/frc/robot/subsystems/ExampleSubsystem.java
+++ src/main/java/frc/robot/subsystems/ExampleSubsystem.java
@@ -4,8 +4,9 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -19,8 +20,9 @@ public class ExampleSubsystem extends SubsystemBase {
     // Inline construction of command goes here.
     // Subsystem::RunOnce implicitly requires `this` subsystem.
     return runOnce(()
-                       -> {
-                           /* one-time action goes here */
+                       -> { // restores the spark max to factory default 
+                          System.out.println("restored to factory defaults :)");
+                          m_motor.restoreFactoryDefaults();
                        });
   }
 
