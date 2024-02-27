87166b2be86016b29849d30fa0558796e2dae809
amelia-chen <amelia.r.chen@gmail.com>
2024-02-26 20:50:50 -0800

    fix indentations

--- src/main/java/frc/robot/commands/DistanceDriveCommand.java
+++ src/main/java/frc/robot/commands/DistanceDriveCommand.java
@@ -14,47 +14,47 @@ import edu.wpi.first.wpilibj2.command.Command;
  * Command to drive a set distance. Uses Drivetrain subsystem.
  */
 public class DistanceDriveCommand extends Command {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Drivetrain m_drivetrain;
-  private double m_targetDistance;
+    @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+    private final Drivetrain m_drivetrain;
+    private double m_targetDistance;
 
-  /** 
-   * A dead reckoning drive command.
-   * Takes in target distance in meters and drives straight that amount.
-   */ 
-  public DistanceDriveCommand(Drivetrain drivetrain, double targetDistance) {
-    m_drivetrain = drivetrain;
-    m_targetDistance = targetDistance * DrivetrainConstants.METERS_TO_ROTATIONS; // convert meters to motor rotations
-    // System.out.println("Target meters: " + targetDistance + ", target rotations: " + m_targetDistance);
+    /** 
+     * A dead reckoning drive command.
+     * Takes in target distance in meters and drives straight that amount.
+     */ 
+    public DistanceDriveCommand(Drivetrain drivetrain, double targetDistance) {
+        m_drivetrain = drivetrain;
+        m_targetDistance = targetDistance * DrivetrainConstants.METERS_TO_ROTATIONS; // convert meters to motor rotations
+        // System.out.println("Target meters: " + targetDistance + ", target rotations: " + m_targetDistance);
 
-    addRequirements(drivetrain);
-  }
+        addRequirements(drivetrain);
+    }
 
-  @Override
-  public void initialize() {
-    // resets positions of encoders
-    m_drivetrain.setRightEncoder(0);
-    m_drivetrain.setleftEncoder(0);
-  }
+    @Override
+    public void initialize() {
+        // resets positions of encoders
+        m_drivetrain.setRightEncoder(0);
+        m_drivetrain.setleftEncoder(0);
+    }
 
-  @Override
-  public void execute() {
-    double thrust = AutoConstants.TAXI_AUTO_SPEED*Math.signum(m_targetDistance); // drives in the direction of targetDistance
-    m_drivetrain.tankDrive(-thrust, -thrust);
-    // System.out.println("Left encoder position: " + m_drivetrain.getLeftEncoderPosition() + ", Right encoder position: " + m_drivetrain.getRightEncoderPosition());
-  }
+    @Override
+    public void execute() {
+        double thrust = AutoConstants.TAXI_AUTO_SPEED*Math.signum(m_targetDistance); // drives in the direction of targetDistance
+        m_drivetrain.tankDrive(-thrust, -thrust);
+        // System.out.println("Left encoder position: " + m_drivetrain.getLeftEncoderPosition() + ", Right encoder position: " + m_drivetrain.getRightEncoderPosition());
+    }
 
-  @Override
-  public void end(boolean interrupted) {
-    m_drivetrain.stopMotors();
-    m_drivetrain.setRightEncoder(0);
-    m_drivetrain.setleftEncoder(0);
-  }
+    @Override
+    public void end(boolean interrupted) {
+        m_drivetrain.stopMotors();
+        m_drivetrain.setRightEncoder(0);
+        m_drivetrain.setleftEncoder(0);
+    }
 
-  @Override
-  public boolean isFinished() {
-    // calculates if either encoder has moved enough to reach the target distance
-    return (Math.abs(m_drivetrain.getRightEncoderPosition()) > Math.abs(m_targetDistance)
-    || Math.abs(m_drivetrain.getLeftEncoderPosition()) > Math.abs(m_targetDistance)); 
-  }
+    @Override
+    public boolean isFinished() {
+        // calculates if either encoder has moved enough to reach the target distance
+        return (Math.abs(m_drivetrain.getRightEncoderPosition()) > Math.abs(m_targetDistance)
+        || Math.abs(m_drivetrain.getLeftEncoderPosition()) > Math.abs(m_targetDistance)); 
+    }
 }
