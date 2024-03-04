5e71ecf5addee34c0d3aa22a01f5a382a82d025e
riley <rileykjen@gmail.com>
2024-03-03 17:04:47 -0800

    add comments + remove print

--- src/main/java/frc/robot/commands/ArcadeDriveCommand.java
+++ src/main/java/frc/robot/commands/ArcadeDriveCommand.java
@@ -29,12 +29,8 @@ public class ArcadeDriveCommand extends Command {
         double x_val = Math.abs(xJoy) > OperatorConstants.DRIVER_JOYSTICK_DEADBAND ? xJoy : 0.0;
         double y_val = Math.abs(yJoy) > OperatorConstants.DRIVER_JOYSTICK_DEADBAND ? yJoy : 0.0;
 
-        double original = x_val;
-
-        int sign = xJoy > 0 ? 1 : -1;
-        x_val = (x_val*x_val)*sign;
-
-        System.out.println(original + ", " + x_val);
+        int sign = xJoy > 0 ? 1 : -1; // preserve pos/neg
+        x_val = (x_val*x_val)*sign; // square
 
         m_drivetrain.arcadeDrive(OperatorConstants.DRIVE_SPEED_ADJUSTMENT * x_val, OperatorConstants.DRIVE_SPEED_ADJUSTMENT * y_val);
     }
