5edbbfa04169a997fc2780cf2200b91de79a2d96
amelia-chen <amelia.r.chen@gmail.com>
2024-04-06 15:59:53 -0700

    make beam break only print once per second

--- src/main/java/frc/robot/commands/BeamBreakCommand.java
+++ src/main/java/frc/robot/commands/BeamBreakCommand.java
@@ -2,26 +2,35 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Indexer;
+import edu.wpi.first.wpilibj.Timer;
 
 /**
  * Testing command that prints beam break values.
  */
 public class BeamBreakCommand extends Command {
     private Indexer m_indexer;
+    private Timer m_timer;
     
     public BeamBreakCommand(Indexer indexer){
         m_indexer = indexer;
+        m_timer = new Timer();
         addRequirements(indexer);
     }
 
     @Override
     public void initialize() {
+        m_timer.reset();
+        m_timer.start();
     }  
 
     @Override
     public void execute() {
-        m_indexer.printBeamBreak();
-        System.out.println(m_indexer.getEncoderPosition());
+        if (m_timer.get() > 1) {
+            m_indexer.printBeamBreak();
+            System.out.println(m_indexer.getEncoderPosition());
+            m_timer.reset();
+            m_timer.start();
+        }
     }
 
     @Override
